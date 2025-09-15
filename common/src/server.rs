04766e96b5ff3ab6e2f457cf5b46268b791d11d1
EduardoStarZ <pathver@gmail.com>
2025-09-15 20:16:33 -0300

    more server fiddling

--- common/src/server.rs
+++ common/src/server.rs
@@ -2,7 +2,22 @@ use std::net::{Ipv4Addr};
 
 pub struct Server {
     pub id : u8,
-    pub name : String,
+    pub name : &'static str,
     pub ip : Ipv4Addr,
     pub port : u16,
+}
+
+impl Server {
+    pub fn new() -> Server {
+        return Server { 
+            id : 0,
+            name : "Server",
+            ip : Ipv4Addr::new(127, 0, 0, 1),
+            port : 8080
+        };
+    }
+
+    pub fn from(id: u8, name: &'static str, ip: Ipv4Addr, port: u16) -> Server {
+        return Server {id, name, ip, port};
+    }
 }
