6f0be9991773b0d0e9ae9dbed009bf5f7d0d6773
EduardoStarZ <pathver@gmail.com>
2025-11-06 08:57:21 -0300

    string to hex

--- static-fs/src/main.rs
+++ static-fs/src/main.rs
@@ -2,6 +2,7 @@ use std::net::Ipv4Addr;
 use common::server::{Server, ServerID};
 use ntex::web;
 use ntex_files as ntxfs;
+use serde::Deserialize;
 
 #[ntex::main]
 async fn main() -> std::io::Result<()> {
@@ -15,3 +16,44 @@ async fn main() -> std::io::Result<()> {
 
     server.bind((options.ip, options.port))?.run().await
 }
+
+#[derive(Deserialize)]
+struct FileForm {
+    pub name : String,
+    pub extension : String,
+    pub destination : String,
+    pub contents : String
+}
+
+#[web::post("/send")]
+async fn get_file(form : web::types::Form<FileForm>) -> web::HttpResponse {
+   let file : String = format!("{}.{}", form.name.clone(), form.extension.clone());
+
+   let destination : String = form.destination.clone();
+
+   let str_contents : String = form.contents.clone();
+
+   let contents : Vec<u8> = hex_to_u8(&str_contents);
+
+    return web::HttpResponse::Ok().finish();
+}
+
+fn hex_to_u8(hex : &String) -> Vec<u8> {
+    let mut hexes : Vec<u8> = Vec::new();
+    let mut copied_str = hex.chars().rev().collect::<String>();
+
+    for _ in 0..(hex.len() / 2) {
+        let sub_str : String = format!("{}{}", copied_str.pop().unwrap(), copied_str.pop().unwrap());
+
+        let value : u8 = match u8::from_str_radix(sub_str.as_str(), 16) {
+            Ok(value) => value,
+            Err(err) => {
+                println!("Error while parsing a hexadecimal value to a unsigned integer of 8 bits! Returning a u8::MIN value and displaying backtrace: {}", err.to_string()); 
+                u8::MIN 
+            }
+        };
+        hexes.push(value);
+    }
+    return hexes;
+}
+
